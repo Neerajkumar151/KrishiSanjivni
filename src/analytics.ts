aa4f1d45c68511fcb4bcef62114bd44c0391cc8e
NEERAJ KUMAR <thakurneerajkumar17@gmail.com>
2026-01-30 11:41:51 +0530

    update

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,6 +1,16 @@
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 export const trackPageView = (path: string) => {
-  // @ts-ignore
-  window.gtag("config", "G-NS1CKYFGYJ", {
+  if (!window.gtag) return;
+
+  window.gtag("event", "page_view", {
     page_path: path,
+    page_location: window.location.href,
+    page_title: document.title,
+    debug_mode: true, // REQUIRED for DebugView
   });
 };
